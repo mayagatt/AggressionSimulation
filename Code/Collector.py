308cb3b2e72c4206eabd054bf2cd87dde5d002d0
maya gatt <maya.harari@mail.huji.ac.il>
2022-12-12 15:38:08 +0200

    writer collector - going over all files in the raw data dir and writing their last row to a collected file

---
+++ Code/Collector.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+if __name__ == "__main__":
+    import argparse
+
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-d', type=str, required=True, help='Raw data dir')
+    PARSER.add_argument('-o', type=str, required=True, help='Out file')  # without a suffix!
+    parser = PARSER.parse_args()
+    raw_data_dir = parser.d
+    out_file = parser.o
+
+    collected_df = pd.DataFrame()
+    i = 0
+    for file in os.listdir(raw_data_dir):
+        filename = raw_data_dir + "/" + os.fsdecode(file)
+        if filename.endswith(".csv"):
+            ss_data_df = pd.read_csv(filename, index_col=0)[-2:-1]
+            if i == 0:
+                collected_df = ss_data_df
+                i += 1
+            else:
+                collected_df = pd.concat([collected_df, ss_data_df], ignore_index=True)
+    collected_df.to_csv(out_file + 'csv')

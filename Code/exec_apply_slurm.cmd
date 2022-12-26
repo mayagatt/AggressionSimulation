aff1114f111b54cd11b322963eadec8514e723c7
maya gatt <maya.harari@mail.huji.ac.il>
2022-12-26 17:49:08 +0200

    add directories per E

--- Code/exec_apply_slurm.cmd
+++ Code/exec_apply_slurm.cmd
@@ -9,16 +9,17 @@
 #SBATCH -t 119:59:00
 #SBATCH --mem=4000
 
-OFFSET=2999
+OFFSET=-1
 LINE_NUM=$(echo "$SLURM_ARRAY_TASK_ID + $OFFSET" | bc)
 echo "$LINE_NUM"
 rundir='v_a_vs_deltaL_and_c0'
+E_dir = 'E_1'
 
-outdir="../Data/Raw/${rundir}"
+outdir="../Data/Raw/${rundir}/${E_dir}"
 mkdir -p $outdir
 outfile="$outdir/out_$LINE_NUM"
 setup_file="$outdir/to_run.tsv"
-params_file="$outdir/default_params.json"
+default_params_file="../Data/Raw/${rundir}/default_params.json"
 
 echo "Line $LINE_NUM ; params_file $params_file ; outfile $outfile"
-python3 ./ode_solver.py -o $outfile -d $params_file -n $LINE_NUM -s $setup_file
+python3 ./ode_solver.py -o $outfile -d $default_params_file -n $LINE_NUM -s $setup_file

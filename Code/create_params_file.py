da396f2f31b73ca03fa0012dbf7f8f5d13beb5f4
maya gatt <maya.harari@mail.huji.ac.il>
2023-01-02 11:05:00 +0200

    print file length

--- Code/create_params_file.py
+++ Code/create_params_file.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def change_two_params(setup_file_path, params, param1, param1_arr, param2, param2_arr):
+def change_two_params(params, param1, param1_arr, param2, param2_arr):
     """
     writes to a csv file a DataFrame containing the same value in each row for all the params, accepts param1 and param2.
     For every value of param1 we  match all the values of param2, e.g.:
@@ -28,26 +28,29 @@ def change_two_params(setup_file_path, params, param1, param1_arr, param2, param
     df = (pd.DataFrame(np.repeat(df.values, tot_len, axis=0), columns=df.columns))  # repeat same row in all df
     df[param1] = np.repeat(param1_arr, len(param2_arr))  # replace param1
     df[param2] = np.round(np.tile(param2_arr, len(param1_arr)), 1)  # replace param2, todo: beware of the rounding
-    df.to_csv(setup_file_path)
+    return df, tot_len
 
 
 if __name__ == "__main__":
     import argparse
+
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument('-o', type=str, required=True, help='Output file')
+    PARSER.add_argument('-o', type=str, required=True, help='Output file, including suffix')
     parser = PARSER.parse_args()
     out_file = parser.o
 
-    params = {'log10c0': 1, 'x': 0, 'rhoA': 0.5, 'rhoV': 0.5, 'K': 1, 'K_L': 1, 'E': 1, 'DeltaE': 0.1, 'deltaL': 1,
+    params = {'log10c0': 1, 'x': 0, 'rhoA': 0.5, 'rhoV': 0.5, 'K': 1, 'K_L': 1, 'E': 2, 'DeltaE': 0.1, 'deltaL': 1,
               'gtype': 'hill', 'h': 1, 'tf': 10,
               'is_restart_antibiotics': True}
     min_deltaL = 0
-    max_deltaL = 1
-    tot_deltaL = 101
+    max_deltaL = 0.4
+    tot_deltaL = 201
     min_log10c0 = -3
     max_log10c0 = 3
-    tot_log10c0 = 31
+    tot_log10c0 = 101
     # path = ".\Data\Raw\\v_a_vs_deltaL_and_c0\\to_run.tsv"
     deltaLs = np.linspace(min_deltaL, max_deltaL, tot_deltaL)
     log10c0s = np.linspace(min_log10c0, max_log10c0, tot_log10c0)
-    change_two_params(out_file, params, param1='deltaL', param2='log10c0', param1_arr=deltaLs, param2_arr=log10c0s)
+    df, tot_len = change_two_params(params, param1='deltaL', param2='log10c0', param1_arr=deltaLs, param2_arr=log10c0s)
+    df.to_csv(out_file)
+    print('Param file length: ', tot_len)

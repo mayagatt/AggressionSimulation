2ffa632ca1af17143f3d3df6554b1901a11063cd
maya gatt <maya.harari@mail.huji.ac.il>
2023-01-02 12:58:25 +0200

    make offset and array be given from ouside

--- Code/exec_apply_slurm.cmd
+++ Code/exec_apply_slurm.cmd
@@ -1,6 +1,6 @@
 #!/bin/bash
 # #SBATCH --array=1-1
-#SBATCH --array=1-200
+# #SBATCH --array=1-1000
 #SBATCH -o logs/scan_%A_%a.out
 #SBATCH -N 1 # node count
 #SBATCH -c 1
@@ -9,11 +9,11 @@
 #SBATCH -t 119:59:00
 #SBATCH --mem=4000
 
-OFFSET=-1
+OFFSET=$1
 LINE_NUM=$(echo "$SLURM_ARRAY_TASK_ID + $OFFSET" | bc)
 echo "$LINE_NUM"
 rundir='v_a_vs_deltaL_and_c0'
-E_dir = 'E_1'
+E_dir='E_1'
 
 outdir="../Data/Raw/${rundir}/${E_dir}"
 mkdir -p $outdir
